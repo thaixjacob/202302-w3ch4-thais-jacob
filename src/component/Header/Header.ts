8df4cbffa7b9fce343256734961ee2570dffc72a
`thais` <`tjlannes@gmail.com`>
2023-02-02 20:23:44 +0100

    create header component

---
+++ src/component/Header/Header.ts
@@ -0,0 +1,19 @@
+import Component from '../Component.js';
+
+export default class Header extends Component {
+  #text: string;
+
+  constructor(
+    text: string,
+    parentElement: HTMLElement | null,
+    cssClasses: string = '',
+  ) {
+    super(parentElement, 'header', cssClasses);
+    this.#text = text;
+  }
+
+  render(): void {
+    super.render();
+    this.element.textContent = `<h1>${this.#text}</h1>`;
+  }
+}
